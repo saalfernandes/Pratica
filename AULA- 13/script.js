e7be7f777bc1b679ea5deffafef9b26dab9e3d3e
saalfernandes <dudaf1691@gmail.com>
2024-08-17 16:52:16 -0300

    Add files via upload

---
+++ AULA- 13/script.js
@@ -0,0 +1,105 @@
+// questão1:
+// const num1 = +prompt("escreva o primeiro número: ")
+// const num2 = +prompt("escreva o segundo número: ")
+// const num3 = +prompt("escreva o terceiro número: ")
+
+// const soma = num1 + num2
+
+// if (soma < num3) {
+//   console.log(" a soma é menor!")
+// } else {
+//     console.log("a soma não é menor!")
+// }
+//  questão2:
+
+//const num1 = +prompt("escreva um número: ")
+
+//if (num1 % 2 === 0) {
+  //  console.log("é par")
+//} else {
+  //  console.log("é ímpar")
+//}/
+// questão4:
+//const num1 = +prompt("escreva o primeiro número: ")
+//const num2 = +prompt("escreva o segundo número: ")
+//const num3 = +prompt("escreva o terceiro número: ")
+ 
+//if (num1 === num2) {    
+   // console.log("é igual")
+//} else {
+//    console.log("número C")
+//}
+
+// questão4:
+//const num1 = +prompt("escreva o primeiro número: ")
+//const num2 = +prompt("escreva o segundo número: ")
+//const num3 = +prompt("escreva o terceiro número: ")
+ 
+//if (num1 === num2) {    
+  //  console.log("é igual")
+//} else {
+ //   console.log("número C")
+//}
+
+//questão5:
+//const número = +prompt("escreva o primeiro número: ")
+//if (número >= 0){
+  //  console.log ("o número é positivo")
+//} else {
+  //  console.log("o número é negativo")
+//}
+
+//questão6:
+//const num1 = +prompt("escreva o primeiro número: ")
+//const num2 = +prompt("escreva o segundo número: ")
+
+//if (num1=== 1 && num1=== 1){
+  //  console.log("true")
+//} else{
+  //  console.log("false")
+//}
+
+//questão7:
+//const num1 = +prompt("escreva o primeiro número: ")
+
+//if (num1 % 2 === 0) {
+ //   console.log(num1 + 5)
+//} else{
+  //  console.log(num2 + 8)
+//}
+
+//questão8:
+//const num1 = +prompt("escreva um número: ")
+//const num2 = +prompt("escreva um número: ")
+//const num3 = +prompt("escreva um número: ")
+
+//const maior = Math.max(num1, num2, num3)
+//const menor = Math.min(num1, num2, num3)
+//const meio = (num1 + num2 + num3) - maior - menor
+
+//.log(maior, meio, menor)
+
+//questão9:
+//const genero = prompt("digite seu genero: ")
+//let altura = prompt("digite sua altura: ")
+//let pesoideal
+
+//if(genero === "M"){
+  //  console.log((72.7 * altura) - 58)
+//}else{
+  //  console.log((62.1 * altura) - 44.7)
+//}
+
+//questão10:
+//const peso = +prompt("digite seu peso: ")
+//const altura =+prompt("digite sua altura: ")
+
+//const IMC = peso/ (altura * altura)
+
+//if (IMC < 18.5){
+  //  console.log("abaixo do peso")
+//}else if (IMC < 25) {
+  //  console.log("peso normal")
+//} else if (IMC < 30) {
+  //  console.log("acima do peso")
+//}
